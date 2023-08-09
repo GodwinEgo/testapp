6ccc1e5e3f9347e989463f1f914b1f0c54f9b9ac
GodwinEgo <godwinehile04@gmail.com>
2023-08-09 23:28:22 +0100

    app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="nav">
+      <div className="topNav">
+        <h2>
+          Search across '3.3 crore +' <a href="/">Products & Services</a>
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
